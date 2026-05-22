a277a320e99a8d21906871d16ed5debf4e59653e
VANGUARD1708 <ab.chidubem@gmail.com>
2026-05-22 13:41:14 +0000

    G

--- artifacts/spark/src/App.tsx
+++ artifacts/spark/src/App.tsx
@@ -1,18 +1,11 @@
 import { Router, Route, Switch } from "wouter";
-
-function Home() {
-  return (
-    <div className="min-h-screen bg-black text-lime-500 flex items-center justify-center text-5xl">
-      WOUTER WORKS
-    </div>
-  );
-}
+import Dashboard from "@/pages/dashboard";
 
 export default function App() {
   return (
     <Router>
       <Switch>
-        <Route path="/" component={Home} />
+        <Route path="/" component={Dashboard} />
       </Switch>
     </Router>
   );
