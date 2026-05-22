3c0ad4cc19cd4fec9c1b018c42597274648deded
VANGUARD1708 <ab.chidubem@gmail.com>
2026-05-22 13:53:02 +0000

    Yy

--- artifacts/spark/src/App.tsx
+++ artifacts/spark/src/App.tsx
@@ -1,12 +1,17 @@
 import { Router, Route, Switch } from "wouter";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import Dashboard from "@/pages/dashboard";
+
+const queryClient = new QueryClient();
 
 export default function App() {
   return (
-    <Router>
-      <Switch>
-        <Route path="/" component={Dashboard} />
-      </Switch>
-    </Router>
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <Switch>
+          <Route path="/" component={Dashboard} />
+        </Switch>
+      </Router>
+    </QueryClientProvider>
   );
 }
